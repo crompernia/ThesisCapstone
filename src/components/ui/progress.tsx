78c290b3d903c84c6ce3152e55d96f9968b20ca4
crom <crompernia@gmail.com>
2025-10-28 12:57:40 +0800

    Fix TypeScript error in progress component for Vercel deployment

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,12 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "@/lib/utils"
 
 /** The Progress component. */
-const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    value?: number
+  }
+>(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
