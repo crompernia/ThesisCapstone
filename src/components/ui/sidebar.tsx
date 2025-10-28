ec7b973520e0f137dbcc3cded778169b3f38c8f2
crom <crompernia@gmail.com>
2025-10-28 13:25:32 +0800

    Fix SidebarTrigger type error in sidebar component for Vercel deployment

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -249,7 +249,10 @@ const Sidebar = React.forwardRef<
 )
 Sidebar.displayName = "Sidebar"
 
-const SidebarTrigger = React.forwardRef(({ className, onClick, ...props }, ref) => {
+const SidebarTrigger = React.forwardRef<
+  React.ElementRef<typeof Button>,
+  React.ComponentPropsWithoutRef<typeof Button>
+>(({ className, onClick, ...props }, ref) => {
   const { toggleSidebar } = useSidebar()
 
   return (
